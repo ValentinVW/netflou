12a2953d87544bb58a3e09264261bd7ca726a0f9
Valentin VW <valentinvanweydevelt@hotmail.com>
2023-08-30 18:00:18 +0200

    tailwind-css-bug-fix

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,13 @@
 <script setup>
-import Header from "./components/Header.vue";
-import Content from "./components/Content.vue";
-import Footer from "./components/Footer.vue";
+import movies from "./components/movies.vue";
+import search from "./components/searchBar.vue";
+import series from "./components/series.vue";
 </script>
 
 <template>
-  <Header />
-  <Content />
-  <Footer />
+  <div class="fixed w-full h-screen bg-black">
+    <search />
+    <movies />
+    <series />
+  </div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
